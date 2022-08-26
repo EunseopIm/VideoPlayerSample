043b240f981dea28ec9f13f80ff7e23fa233aa50
esleem <esleem@solter.co.kr>
2022-08-26 17:11:40 +0900

    -

--- app/src/main/java/com/eee/videoapp/MainActivity.kt
+++ app/src/main/java/com/eee/videoapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.eee.videoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eee.videoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
